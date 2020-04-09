9c7f0192d578db038953c358f9b275cde8850a29
Arnon <arnon.b@zooz.com>
2020-04-09 08:00:50 +0300

    removed console log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,6 @@ const forecast = (lat, long, callback) => {
             callback("Unable to find location", undefined)
         }
         else {
-            console.log(body.daily.data[0])
             const foreCastTomorrow = body.daily.summary
             const forecastMessage = "It is currently " + body.currently.temperature + " degrees. There is a " + body.currently.precipProbability  + " chance of rain. The highest temperature will be " +  body.daily.data[0].temperatureHigh + " degrees."
             callback(undefined, forecastMessage)
